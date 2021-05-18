479ca5b1552e713b26d254fd9391c1af0c03b107
Luka Isailovic <lukaisailovic@gmail.com>
2021-05-18 19:45:51 +0200

    Update comments repository to use mysql

--- src/main/java/rs/raf/web_d7/repositories/CommentRepository.java
+++ src/main/java/rs/raf/web_d7/repositories/CommentRepository.java
@@ -1,29 +1,150 @@
 package rs.raf.web_d7.repositories;
 
 import rs.raf.web_d7.entities.Comment;
+
+import rs.raf.web_d7.entities.Post;
 import rs.raf.web_d7.repositories.interfaces.ICommentRepository;
 
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
-public class CommentRepository implements ICommentRepository {
+public class CommentRepository extends MySqlAbstractRepository implements ICommentRepository {
+
+    private Comment getFromResultSet(ResultSet resultSet) throws SQLException {
+        return new Comment(resultSet.getInt("id"),resultSet.getInt("post_id"), resultSet.getString("author"), resultSet.getString("content"));
+    }
 
     public Comment add(Comment obj) {
-        return null;
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+        try {
+            connection = this.newConnection();
+
+            String[] generatedColumns = {"id"};
+
+            preparedStatement = connection.prepareStatement("INSERT INTO comments (author, content, post_id) VALUES(?, ?, ?)", generatedColumns);
+            preparedStatement.setString(1, obj.getAuthor());
+            preparedStatement.setString(2, obj.getContent());
+            preparedStatement.setInt(3, obj.getPostId());
+            preparedStatement.executeUpdate();
+            resultSet = preparedStatement.getGeneratedKeys();
+
+            if (resultSet.next()) {
+                obj.setId(resultSet.getInt(1));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            this.closeStatement(preparedStatement);
+            this.closeResultSet(resultSet);
+            this.closeConnection(connection);
+        }
+        return obj;
     }
 
     public List<Comment> all() {
-        return null;
+        List<Comment> comments = new ArrayList<>();
+
+        Connection connection = null;
+        Statement statement = null;
+        ResultSet resultSet = null;
+        try {
+            connection = this.newConnection();
+
+            statement = connection.createStatement();
+            resultSet = statement.executeQuery("select * from comments");
+            while (resultSet.next()) {
+                comments.add(getFromResultSet(resultSet));
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            this.closeStatement(statement);
+            this.closeResultSet(resultSet);
+            this.closeConnection(connection);
+        }
+
+        return comments;
     }
 
     public Comment find(Integer id) {
-        return null;
+        Comment comment = null;
+
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+        try {
+            connection = this.newConnection();
+
+            preparedStatement = connection.prepareStatement("SELECT * FROM comments where id = ?");
+            preparedStatement.setInt(1, id);
+            resultSet = preparedStatement.executeQuery();
+            if(resultSet.next()) {
+                comment = getFromResultSet(resultSet);
+            }
+
+            resultSet.close();
+            preparedStatement.close();
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            this.closeStatement(preparedStatement);
+            this.closeResultSet(resultSet);
+            this.closeConnection(connection);
+        }
+
+        return comment;
     }
 
     public List<Comment> findByPost(Integer id) {
-        return null;
+        List<Comment> comments = new ArrayList<>();
+
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+        try {
+            connection = this.newConnection();
+
+            preparedStatement = connection.prepareStatement("SELECT * FROM comments where post_id = ?");
+            preparedStatement.setInt(1, id);
+            resultSet = preparedStatement.executeQuery();
+            preparedStatement.setInt(1, id);
+            while (resultSet.next()) {
+                comments.add(getFromResultSet(resultSet));
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            this.closeStatement(preparedStatement);
+            this.closeResultSet(resultSet);
+            this.closeConnection(connection);
+        }
+
+        return comments;
     }
 
     public void delete(Integer id) {
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        try {
+            connection = this.newConnection();
 
+            preparedStatement = connection.prepareStatement("DELETE FROM comments where id = ?");
+            preparedStatement.setInt(1, id);
+            preparedStatement.executeUpdate();
+
+            preparedStatement.close();
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            this.closeStatement(preparedStatement);
+            this.closeConnection(connection);
+        }
     }
 }
