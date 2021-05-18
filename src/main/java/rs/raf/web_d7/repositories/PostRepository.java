387eb22717e3dad6c68c6f3cd943337ec860440b
Luka Isailovic <lukaisailovic@gmail.com>
2021-05-18 19:36:33 +0200

    Update post repository to use mysql

--- src/main/java/rs/raf/web_d7/repositories/PostRepository.java
+++ src/main/java/rs/raf/web_d7/repositories/PostRepository.java
@@ -3,22 +3,113 @@ package rs.raf.web_d7.repositories;
 import rs.raf.web_d7.entities.Post;
 import rs.raf.web_d7.repositories.interfaces.IPostRepository;
 
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
-public class PostRepository implements IPostRepository {
+public class PostRepository extends MySqlAbstractRepository implements IPostRepository {
     public Post add(Post obj) {
-        return null;
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+        try {
+            connection = this.newConnection();
+
+            String[] generatedColumns = {"id"};
+
+            preparedStatement = connection.prepareStatement("INSERT INTO posts (author,title, content) VALUES(?, ?, ?)", generatedColumns);
+            preparedStatement.setString(1, obj.getAuthor());
+            preparedStatement.setString(2, obj.getTitle());
+            preparedStatement.setString(3, obj.getContent());
+            preparedStatement.executeUpdate();
+            resultSet = preparedStatement.getGeneratedKeys();
+
+            if (resultSet.next()) {
+                obj.setId(resultSet.getInt(1));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            this.closeStatement(preparedStatement);
+            this.closeResultSet(resultSet);
+            this.closeConnection(connection);
+        }
+        return obj;
     }
 
     public List<Post> all() {
-        return null;
+        List<Post> posts = new ArrayList<>();
+
+        Connection connection = null;
+        Statement statement = null;
+        ResultSet resultSet = null;
+        try {
+            connection = this.newConnection();
+
+            statement = connection.createStatement();
+            resultSet = statement.executeQuery("select * from posts");
+            while (resultSet.next()) {
+                posts.add(new Post(resultSet.getInt("id"), resultSet.getString("author"), resultSet.getString("title"), resultSet.getString("content")));
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            this.closeStatement(statement);
+            this.closeResultSet(resultSet);
+            this.closeConnection(connection);
+        }
+
+        return posts;
     }
 
     public Post find(Integer id) {
-        return null;
+        Post post = null;
+
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+        try {
+            connection = this.newConnection();
+
+            preparedStatement = connection.prepareStatement("SELECT * FROM posts where id = ?");
+            preparedStatement.setInt(1, id);
+            resultSet = preparedStatement.executeQuery();
+            if(resultSet.next()) {
+                post = new Post(resultSet.getInt("id"),resultSet.getString("author"),resultSet.getString("title"),resultSet.getString("content"));
+            }
+
+            resultSet.close();
+            preparedStatement.close();
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            this.closeStatement(preparedStatement);
+            this.closeResultSet(resultSet);
+            this.closeConnection(connection);
+        }
+
+        return post;
     }
 
     public void delete(Integer id) {
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        try {
+            connection = this.newConnection();
 
+            preparedStatement = connection.prepareStatement("DELETE FROM posts where id = ?");
+            preparedStatement.setInt(1, id);
+            preparedStatement.executeUpdate();
+
+            preparedStatement.close();
+            connection.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            this.closeStatement(preparedStatement);
+            this.closeConnection(connection);
+        }
     }
 }
