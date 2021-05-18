5bddcd17221772f2bc9fbd7e7e8c774d95fe7910
Luka Isailovic <lukaisailovic@gmail.com>
2021-05-18 15:51:10 +0200

    Create entities

---
+++ src/main/java/rs/raf/web_d7/entities/Comment.java
@@ -0,0 +1,68 @@
+package rs.raf.web_d7.entities;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+public class Comment {
+
+    private Integer id;
+
+    private Integer postId;
+
+    @NotNull(message = "Author is required")
+    @NotEmpty(message = "Author is required")
+    private String author;
+
+    @NotNull(message = "Content is required")
+    @NotEmpty(message = "Content is required")
+    private String content;
+
+    public Comment(Integer id, Integer postId,  String author, String content) {
+        this.id = id;
+        this.postId = postId;
+        this.author = author;
+        this.content = content;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getPostId() {
+        return postId;
+    }
+
+    public void setPostId(Integer postId) {
+        this.postId = postId;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    @Override
+    public String toString() {
+        return "Comment{" +
+                "id=" + id +
+                ", postId=" + postId +
+                ", author='" + author + '\'' +
+                ", content='" + content + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/rs/raf/web_d7/entities/Post.java
@@ -0,0 +1,71 @@
+package rs.raf.web_d7.entities;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+public class Post {
+
+    private Integer id;
+
+    @NotNull(message = "Author is required")
+    @NotEmpty(message = "Author is required")
+    private String author;
+
+    @NotNull(message = "Title is required")
+    @NotEmpty(message = "Title is required")
+    private String title;
+
+    @NotNull(message = "Content is required")
+    @NotEmpty(message = "Content is required")
+    private String content;
+
+
+    public Post(Integer id, String author, String title, String content) {
+        this.id = id;
+        this.author = author;
+        this.title = title;
+        this.content = content;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    @Override
+    public String toString() {
+        return "Post{" +
+                "id=" + id +
+                ", author='" + author + '\'' +
+                ", title='" + title + '\'' +
+                ", content='" + content + '\'' +
+                '}';
+    }
+}
